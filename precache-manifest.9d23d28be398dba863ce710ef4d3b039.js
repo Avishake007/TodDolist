21e081169e108077ae9f60523018fb6a0b8bf2f4
Avishake <Avishake007>
2020-09-21 09:47:09 +0530

    Updates

---
+++ precache-manifest.9d23d28be398dba863ce710ef4d3b039.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d78e7b63b86b2c5d0c524110055ab05",
+    "url": "/TodDolist/index.html"
+  },
+  {
+    "revision": "f63a2b95d7ff238c6baf",
+    "url": "/TodDolist/static/css/main.a6a27117.chunk.css"
+  },
+  {
+    "revision": "c04aff44dcd11dfb34c8",
+    "url": "/TodDolist/static/js/2.4f5d1831.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TodDolist/static/js/2.4f5d1831.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f63a2b95d7ff238c6baf",
+    "url": "/TodDolist/static/js/main.b40780ac.chunk.js"
+  },
+  {
+    "revision": "f9b8629efd1f1455368b",
+    "url": "/TodDolist/static/js/runtime-main.fc484b25.js"
+  }
+]);
